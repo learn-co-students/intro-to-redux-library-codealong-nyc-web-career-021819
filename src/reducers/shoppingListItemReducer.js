5a4fe46561211d6b231cd3c6120e0a7ccf3b27eb
David Korff <dkorff12@gmail.com>
2019-05-09 15:31:43 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,15 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
+const defaultState = {items: []}
+
+export default function shoppingListItemReducer(state = defaultState, action) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+			console.log('Current state.items length %s', state.items.length);
+      console.log('Updating state.items length to %s', state.items.length + 1);
+			return {items: state.items.concat(state.items.length + 1)}
 
 		default:
+			console.log('Initial state.items length: %s', state.items.length);
 			return state;
 	}
 }
